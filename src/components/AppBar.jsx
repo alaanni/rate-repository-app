00c6e6bcd4697d1da176b46b17667c1e352810d5
Anni Ala-Mattinen <anni.alamattinen@gmail.com>
2022-01-03 22:48:14 +0200

    exercises 10.6-10.7.

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,19 +1,35 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, ScrollView } from 'react-native';
+import { Link } from 'react-router-native';
 import Constants from 'expo-constants';
-import AppBarTab from './AppBarTab';
+import theme from '../theme';
+import Text from './Text';
 
 const styles = StyleSheet.create({
   container: {
     paddingTop: Constants.statusBarHeight,
-    backgroundColor: '#24292e'
+    backgroundColor: '#24292e',
+    flexDirection: 'row'
   },
-  // ...
+  appBarTitle: {
+    color: 'white',
+    fontWeight: theme.fontWeights.bold,
+    fontSize: 20,
+    padding: 25,
+  },
 });
 
 const AppBar = () => {
   return <View style={styles.container}>
-    <AppBarTab title="Repositories" />
+    <ScrollView horizontal>
+      <Link to="/">
+        <Text style={styles.appBarTitle}>Repositories</Text>
+      </Link>
+      <Link to="/signin">
+        <Text style={styles.appBarTitle}>Sing in</Text>
+      </Link>
+    </ScrollView>
+    
   </View>;
 };
 
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
+import { Route, Switch, Redirect } from 'react-router-native';
+
 import RepositoryList from './RepositoryList';
+import SignIn from './SignIn';
 import AppBar from './AppBar';
 
 const styles = StyleSheet.create({
     container: {
     backgroundColor: '#e1e4e8',
     flex: 1
-    // ...
     }
   });
 
@@ -15,7 +17,15 @@ const Main = () => {
   return (
     <View style={styles.container}>
       <AppBar />
-      <RepositoryList />
+      <Switch>
+        <Route path="/signin" exact>
+          <SignIn />
+        </Route>
+        <Route path="/" exact>
+          <RepositoryList />
+        </Route>
+        <Redirect to="/" />
+      </Switch>
     </View>
   );
 };
