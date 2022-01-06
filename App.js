350fe401a5c9dedb052b33c9f5154ae235558e4e
Anni Ala-Mattinen <anni.alamattinen@gmail.com>
2022-01-06 14:48:28 +0200

    set environment variables

--- App.js
+++ App.js
@@ -3,10 +3,13 @@ import { NativeRouter } from 'react-router-native';
 import { ApolloProvider } from '@apollo/client';
 import Main from './src/components/Main';
 import createApolloClient from './src/utils/apolloClient';
+import Constants from 'expo-constants';
 
 const apolloClient = createApolloClient();
 
 const App = () => {
+  console.log(Constants.manifest);
+
   return (
       <NativeRouter>
         <ApolloProvider client={apolloClient}>
--- src/utils/apolloClient.js
+++ src/utils/apolloClient.js
@@ -1,8 +1,9 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import Constants from 'expo-constants';
 
 const httpLink = createHttpLink({
   // Replace the IP address part with your own IP address!
-  uri: 'http://172.20.10.2:5000/graphql',
+  uri: Constants.manifest.extra.APOLLO_URI,
 });
 
 const createApolloClient = () => {
